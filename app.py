d7de2942d4271568272ba01ce55276e695153847
VirginiaCarrizo <virginia.carrizo@terragene.com>
2024-11-21 09:54:38 -0300

    autocompletar input

--- app.py
+++ app.py
@@ -18,6 +18,9 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 import os
 from dotenv import load_dotenv
 
